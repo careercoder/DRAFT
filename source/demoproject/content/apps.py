a31d0dcd8a6dbf774c357b5db501ceb63ff7fa0e
Jody Fitzpatrick <jody.lee.fitzpatrick@gmail.com>
2019-10-21 16:30:24 -0400

    more pre work
    more pre work and planning

--- source/demoproject/content/apps.py
+++ source/demoproject/content/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class ContentConfig(AppConfig):
     name = 'content'
-
-    swish = dict()
-    swish['component'] = dict()
-    swish['component'] =
---
+++ source/demoproject/content/components/display.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+class ContentComponent:
+
+    def __init__(self):
+        pass
+
+    def render(self, request):
+        pass
+
+
