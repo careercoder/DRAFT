a316dcc48b05096583d569a7b15ad1d33b9e0e05
Jody Fitzpatrick <jody.lee.fitzpatrick@gmail.com>
2019-10-21 21:17:22 -0400

    more blah
    more standard stuff preparing for pre alpha

--- source/demoproject/swish/models/component.py
+++ source/demoproject/swish/models/component.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Component(models.Model):
+
+    name = models.CharField(max_length=150)
+    path = models.CharField(max_length=1064)
+
+    def __str__(self):
+        return self.name
+
+
